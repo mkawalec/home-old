06cb3f62d6f729e10acfa1d7400efe0981b8a83f
Michal Kawalec <mkawalec@lavabit.com>
2012-11-08 17:00:49 +0100

    Corrected the runserver

--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 from gevent.wsgi import WSGIServer
-from mama import app
+from cal import app
 
-http_server = WSGIServer(('', 80), app)
+http_server = WSGIServer(('', 6666), app)
 http_server.serve_forever()
