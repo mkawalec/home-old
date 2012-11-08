8387500deeecd4a1f7aad7dcc55585bac323e072
Michal Kawalec <mkawalec@lavabit.com>
2012-11-08 17:03:46 +0100

    Added some comments into runserver and removed the unneeded deployment files.

--- runserver.py
+++ runserver.py
@@ -1,5 +1,13 @@
 from gevent.wsgi import WSGIServer
 from cal import app
 
-http_server = WSGIServer(('', 6666), app)
-http_server.serve_forever()
+if __name__ == '__main__':
+    # Comment the following two lines if you intend to
+    # run the app in the debug mode (which is probably the
+    # only thing you should be doing at this stage)
+    http_server = WSGIServer(('', 6666), app)
+    http_server.serve_forever()
+
+    # and uncomment this for debug
+    # app.run(debug=True)
+
